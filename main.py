86be4abdd750fdce3fb60b4832ea86cc5d070d39
rykumar13 <rykumar13@gmail.com>
2021-06-16 01:04:19 +0100

    determine insta unfollow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import instaloader
 import requests
 import auth
 
@@ -28,7 +30,43 @@ def send_message(message):
     print(response.status_code)
 
 
+def get_unfollows():
+
+    # login
+    L = instaloader.Instaloader()
+    L.login(auth.insta_username, auth.insta_password)
+    profile = instaloader.Profile.from_username(L.context, auth.insta_username)
+
+    # retrieve & store current followers to file
+    current_file = open('current.txt', 'w')
+    current_followers = []
+    for follower in profile.get_followers():
+        current_file.write(follower.username + '\n')
+        current_followers.append(follower.username)
+    current_file.close()
+
+    # open current followers from file
+    current_followers = open('current.txt').readlines()
+
+    # open old followers from file
+    old_followers = open('old.txt').readlines()
+
+    # calculate differences in followers
+    s = set(current_followers)
+    difference = [x for x in old_followers if x not in s]
+
+    # find unfollows
+    for x in difference:
+        if x in old_followers and x not in current_followers:
+            unfollwer = x.strip('\n')
+            send_message(f'{unfollwer} unfollowed you.')
+
+    # store current followers into old file
+    old_file = open('old.txt', 'w')
+    for follower in current_followers:
+        old_file.write(follower)
+    old_file.close()
+
+
 if __name__ == '__main__':
-    # get_updates()
-    # test_bot()
-    send_message('Hello this is a test notification')
+    get_unfollows()
